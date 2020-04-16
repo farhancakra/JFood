656be244e09db8de5162f001208e17bcbb2b1c5c
farhancakra <muhammad.farhan712@ui.ac.id>
2020-04-16 18:01:52 +0700

    Modul 8

--- src/main/java/MuhammadFarhan/JFood/CustomerController.java
+++ src/main/java/MuhammadFarhan/JFood/CustomerController.java
@@ -3,29 +3,17 @@ import java.util.ArrayList;
 
 public class CustomerController {
     private Customer customer;
-    public Customer loginCustomer(String name, String email, String password){
+
+    public Customer loginCustomer(String name, String email, String password) {
         DatabaseCustomer.CustomerLogin(email, password);
         return customer;
     }
 
-    public Customer registerCustomer(String name, String password){
+    public Customer registerCustomer(String name, String password) {
         return null;
     }
 
-    public Customer getCustomerById(int id){
+    public Customer getCustomerById(int id) {
         return null;
-    }
-
-    public ArrayList<Food> getFoodBySeller(int sellerid){
-
-    }
-
-    public ArrayList<Food> getFoodByCategory(FoodCategory category){
-
-    }
-
-    public Food addFood(String name, int price, FoodCategory category, int sellerid) {
-
-
     }
 }
